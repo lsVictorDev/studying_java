d32fdfcc633c8be89134971c15a236acb18a2795
Victor Lemos <132801215+lsVictorDev@users.noreply.github.com>
2026-02-12 21:51:50 -0300

    primeiro commit

---
+++ curso_java_programacao/src/Aula1.java
@@ -0,0 +1,16 @@
+public class Aula1 {
+
+	public static void main(String[] args) {
+		// Aula 1 - Me apresentar com meu primeiro "Hello World" em Java.
+		
+		String name = "Victor";
+		
+		int age = 21;
+		int year = 2026;
+		
+		System.out.println("Hello World!");
+		System.out.printf("My name is %s, have %d years old, and I've decided to start %d by learning Java.", name, age, year);
+		
+	}
+
+}
